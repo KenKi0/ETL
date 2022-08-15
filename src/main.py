638fedc16d326a64d25b8a2685175499bdf4cb29
kenki <ilsafganiev2222@gmail.com>
2022-08-15 23:54:09 +0300

    fixes in main.py

--- src/main.py
+++ src/main.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     if args.init:
         parts = [PartName.films]
     else:
-        parts = [PartName.persons]
+        parts = [PartName.films, PartName.films_persons, PartName.films_genres, PartName.persons]
 
     while True:
         start_etl_process(parts, args.ex_batch_size, args.ld_batch_size)
