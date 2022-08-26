65a6fc5291a4cc15ed362ea87ba615decfed0915
kenki <ilsafganiev2222@gmail.com>
2022-08-26 17:43:09 +0300

    bugfix

--- src/etl/scheme.py
+++ src/etl/scheme.py
@@ -2,7 +2,7 @@ import uuid
 from typing import Optional
 
 import orjson
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 def orjson_dumps(value, *, default):
@@ -32,13 +32,20 @@ class FilmScheme(BaseScheme):
     id: uuid.UUID  # noqa: VNE003
     title: str
     description: Optional[str]
-    imdb_rating: Optional[float] = 0
+    imdb_rating: Optional[float]
     director: Optional[list[dict]]
     actors: Optional[list[dict]] = []
     writers: Optional[list[dict]]
     actors_names: Optional[list[str]]
     writers_names: Optional[list[str]]
     genre: Optional[list[dict]]
+
+    @validator('imdb_rating')
+    def rating_isnt_none(cls, v):
+        if v is None:
+            v = 0
+        return v
+
 
 
 class PersonScheme(BaseScheme):
