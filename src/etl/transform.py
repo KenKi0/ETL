6d6fc79ad7515eabcfee32e99e1f7b0f92fdc6b5
kenki <ilsafganiev2222@gmail.com>
2022-09-07 23:54:29 +0300

    добавлен новое поле для фильмов, так же изменена схема индекса movies

--- src/etl/transform.py
+++ src/etl/transform.py
@@ -1,4 +1,5 @@
 """ Transform parts logic"""
+import datetime
 import logging
 from typing import Iterator, Protocol
 
@@ -8,6 +9,8 @@ from etl.extract import Extracting
 from utils import TransformedData
 
 logger = logging.getLogger(__name__)
+
+DAYS_IN_YEAR = 365
 
 
 class Transform(Protocol):
@@ -39,6 +42,11 @@ class ElasticTransformer:
         """
         films_data = self.data['films']
         while film := next(films_data):
+            if film_date := film.get('creation_date'):
+                is_for_only_sub = datetime.datetime.utcnow() - film_date <= datetime.timedelta(days=3*DAYS_IN_YEAR)
+                film['only_sub'] = is_for_only_sub
+            else:
+                film['only_sub'] = False
             serialized_film = scheme.FilmScheme(**film)
             es_action = {
                 '_op_type': 'index',
